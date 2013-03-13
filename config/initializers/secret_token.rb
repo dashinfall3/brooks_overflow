d6ec3c83c5815bea38e18d00f84a7fde07e74702
Apprentice <brooksmason@gmail.com>
2013-03-13 09:50:48 -0700

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrooksOverflow::Application.config.secret_token = 'c04648d07ce598b54c6ed469abb1def39b423f0028ee8c6af8c63729d096a8abca42cd13dd50ed5c20aa5b70c96523f9e2f26bbdf258eb83cab40fd052f29b5b'
